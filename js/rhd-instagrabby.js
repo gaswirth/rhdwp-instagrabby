c39529ba87e57d8bc622d238a7efe2faee4a79f7
Nick Gaswirth <nick@roundhouse-designs.com>
2015-05-20 12:31:53 -0400

    Responsive fixes

--- js/rhd-instagrabby.js
+++ js/rhd-instagrabby.js
@@ -4,16 +4,13 @@
    ========================================================================== */
 
 (function($){
-	// Cycle2 Initialization
-	$('.rhd-instagrabby').cycle({
-		slides: '> li',
-		prev: '.cycle-prev',
-		next: '.cycle-next',
-		fx: 'carousel',
-		timeout: 0,
-		carouselFluid: true,
-		allowWrap: false,
-		swipe: true
+	var newVis;
+	var oldVis = parseInt( $('.rhd-instagrabby').attr('data-cycle-carousel-visible'), 10 );
+
+	$('.rhd-instagrabby').on('cycle-bootstrap', function(e, opts, API) {
+		checkWindowSize( opts );
+
+		$(window).resize( function(){ checkWindowSize(opts); } );
 	});
 
 	// jQuery animation fallback
@@ -21,6 +18,25 @@
 		$(".rhd-instagrabby-container").hover(function(){
 			$(this).children('.rhd-instagrabby-pager').fadeToggle();
 		});
-		alert( 'transitions disabled' );
 	}
+
+	function checkWindowSize( opts ){
+		var w = $(window).width();
+
+		if ( w > 720 && w < 800 )
+			newVis = 5;
+		else if ( w > 640 && w < 720 )
+			newVis = 4;
+		else if ( w < 640 )
+			newVis = 3;
+		else
+			newVis = false;
+
+		if ( newVis !== false ) {
+			opts.carouselVisible = newVis;
+		} else {
+			newVis = oldVis;
+		}
+	}
+
 })(jQuery);
